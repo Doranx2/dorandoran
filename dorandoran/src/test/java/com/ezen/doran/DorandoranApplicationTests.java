e6167b372220414ea5bdaf126582ca6bcbec6a2b
Haein-Han <godls1131@gmail.com>
2022-12-26 15:42:40 +0900

    start

---
+++ dorandoran/src/test/java/com/ezen/doran/DorandoranApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ezen.doran;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DorandoranApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
