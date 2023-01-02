278a4b8450fe675f5dd638e09a76a1aba05fdb70
SOOSUGAR <sj720209@gmail.com>
2023-01-02 16:44:15 +0900

    0102_UserDTO_추가

---
+++ dorandoran/src/main/java/com/ezen/doran/dto/UserDTO.java
@@ -0,0 +1,18 @@
+package com.ezen.doran.dto;
+
+import lombok.Data;
+
+@Data
+public class UserDTO {
+	private int userNo;
+	private String userNm;
+	private String userId;
+	private String userPw;
+	private int userAge;
+	private String userGen;
+	private String userEmail;
+	private String userLoc;
+	private String userNick;
+	private String userRole;
+	
+}
