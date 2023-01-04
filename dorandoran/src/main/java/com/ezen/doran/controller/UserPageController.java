6003b5189fdfce074f1dadc3988c55ce6e7d393c
SOOSUGAR <sj720209@gmail.com>
2023-01-04 15:52:40 +0900

    0104_updateUserPage_추가

--- dorandoran/src/main/java/com/ezen/doran/controller/UserPageController.java
+++ dorandoran/src/main/java/com/ezen/doran/controller/UserPageController.java
@@ -1,8 +1,25 @@
 package com.ezen.doran.controller;
 
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import com.ezen.doran.service.userPage.UserPageService;
+
 public class UserPageController {
 	
-	// selectUser(int)
+	@Autowired
+	UserPageService UserPageService;
+	
+	// selectUser
+	@RequestMapping(value = "/userPage", method = RequestMethod.GET)
+	public String selectUser(HttpServletRequest request) {
+		
+		return "userPage/mypage";
+	}
+	
 	
 	
 	// updateUser(UserDTO)
